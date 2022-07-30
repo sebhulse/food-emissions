d7f426e949333ade442c6b65d47a2aaa69f69cb4
Sebastian Hulse <seb.hulse@yahoo.co.uk>
2022-07-30 17:02:51 +0100

    add error checking and results page

--- js/app.js
+++ js/app.js
@@ -1,22 +1,37 @@
-function submit() {
+const submit = () => {
   const veganBreakfasts = document.getElementById("vegan-breakfasts");
   console.log(veganBreakfasts.value);
-  warningMessage(
-    "Addition Error",
-    "Please ensure that your Days of <em>Breakfasts</em> add up to your 'Total days of trip'."
-  );
-}
+  warningMessage("Evening Meals", camelCase("Evening Meals"));
+};
 
-const warningMessage = (heading, message) => {
+const warningMessage = (sectionText, sectionId) => {
   let warningPlaceholder = document.getElementById("warningPlaceholder");
+  let warningSubheadingPlaceholder = document.getElementById(
+    `${sectionId}WarningPlaceholder`
+  );
   const wrapper = document.createElement("div");
   wrapper.innerHTML = [
     `<div class="alert alert-danger" role="alert">`,
-    `<h4>${heading}</h4>`,
-    `${message}`,
+    `<h4>Oops, please check your numbers</h4>`,
+    `Please ensure that your total <strong><em>${sectionText}</em></strong> days (all diets) add up to your 'Total days of trip'.`,
+    `</div>`,
+  ].join("");
+  const wrapperSubheading = document.createElement("div");
+  wrapperSubheading.innerHTML = [
+    `<div class="alert alert-danger" role="alert">`,
+    `Please ensure that your total <strong><em>${sectionText}</em></strong> days (all diets) add up to your 'Total days of trip'.`,
     `</div>`,
   ].join("");
   warningPlaceholder.innerHTML = wrapper.innerHTML;
+  warningSubheadingPlaceholder.innerHTML = wrapperSubheading.innerHTML;
   warningPlaceholder.focus();
   window.location.href = "/#warningPlaceholder";
+};
+
+const camelCase = (str) => {
+  return str
+    .replace(/(?:^\w|[A-Z]|\b\w)/g, function (word, index) {
+      return index == 0 ? word.toLowerCase() : word.toUpperCase();
+    })
+    .replace(/\s+/g, "");
 };
